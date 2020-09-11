63c37bc0529d168eedb841eccb9e1fb2aeb5fbbf
adhiambokonyango <marykonyango74@gmail.com>
2020-09-11 19:13:39 +0300

    home page

--- src/components/home_page/HomePage.jsx
+++ src/components/home_page/HomePage.jsx
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import NavigationBar from "../../views/admin_page/nav_bar/NavigationBar";
 import {Col, Container, Nav, Navbar, Row} from "react-bootstrap";
-import {FaCartPlus, FaList, FaMailBulk, FaSignOutAlt, FaUser, FaSignInAlt} from "react-icons/fa";
+import {FaCartPlus, FaList, FaMailBulk, FaSignOutAlt, FaUser, FaSignInAlt, FaCircle, FaCogs} from "react-icons/fa";
 import './HomePage.css';
 import './images/sample.jpg';
 
@@ -10,35 +10,35 @@ class HomePage extends Component {
     render() {
         return (
             <div>
-                <div className="top-bar">
                     <Container>
-                        <Row className="first_row">
-                            <Navbar.Collapse id="basic-navbar-nav">
 
-                                <Col sm={8}><FaList Awsha  size={25}/></Col>
-                                <Nav className="mr-auto">
-                                    <Col sm={1}><FaUser size={25}/></Col>
-                                    <Col sm={1}><FaMailBulk size={25}/></Col>
-                                    <Col sm={1}><FaCartPlus size={25}/></Col>
-                                    <Col sm={1}><FaSignInAlt size={25}/></Col>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Row>
                       <div class="card">
                           <div class="card-image">
                               <img src={ require('./images/sample.jpg') } />
                               <a class="btn-floating halfway-fab waves-effect waves-light red">
-                                  <i class="material-icons">add</i>
+                                  <i class="material-icons"><FaCogs /></i>
+
                               </a>
                           </div>
                           <div class="card-content">
                               <span class="card-title">Awsha</span>
-                              <p></p>
+                              <p>
+                                  This product targets project management in government institutions.
+                                  Enables quick analysis of project progress.<br/>Modules available in this
+                                  project include:
+                                  <ul>
+                                      <FaCircle size={8}/>{" "}Branches for easy expansion<br/>
+                                      <FaCircle size={8}/>{" "}Teams<br/>
+                                      <FaCircle size={8}/>{" "}Role Based Access<br/>
+                                      <FaCircle size={8}/>{" "}Permission Based Access<br/>
+                                      <FaCircle size={8}/>{" "}Data Visualization<br/>
+                                  </ul>
+
+                              </p>
                           </div>
                       </div>
 
                     </Container>
-                </div>
             </div>
         );
     }
